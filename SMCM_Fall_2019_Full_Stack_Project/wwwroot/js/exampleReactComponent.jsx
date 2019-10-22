03ce53b9499d83601d9bc3840021766d8eb88d75
Chriswarkidd <48682852+Chriswarkidd@users.noreply.github.com>
2019-10-22 13:31:47 -0400

    Made modifications to initail commit of VS files.

---
+++ SMCM_Fall_2019_Full_Stack_Project/wwwroot/js/exampleReactComponent.jsx
@@ -0,0 +1,30 @@
+﻿class TestReactComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        text: "one"};
+    }
+
+
+    //This isn't supposed to be how it works, but this is the way it currently has to be to work.
+    //Will look into this more later.
+    ajaxTest = (obj) => {
+        $.ajax({ url: "/Home/Test", data: {x: 2} }).done(
+            function (result) {
+                obj.setState({ text: result.test }); 
+            });
+    };
+
+    render() {
+        return (
+            <div>Hello, world! I am a react component! :)
+                <p>{this.state.text}</p>
+                <button className={"btn-primary"} onClick={() => this.ajaxTest(this)}> Press me </button>
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(<TestReactComponent />, document.getElementById('content'));
