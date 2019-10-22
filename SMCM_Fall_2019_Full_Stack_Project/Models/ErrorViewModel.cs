a1e28b8a44651c31243283962a71a343a05fec28
Chriswarkidd <48682852+Chriswarkidd@users.noreply.github.com>
2019-10-22 12:15:41 -0400

    Add initial VS Studio Solution and Files

---
+++ SMCM_Fall_2019_Full_Stack_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SMCM_Fall_2019_Full_Stack_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
