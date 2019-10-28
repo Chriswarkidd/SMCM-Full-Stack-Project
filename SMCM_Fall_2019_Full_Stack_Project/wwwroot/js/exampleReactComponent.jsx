e9eb6944ca8b3c079b8cae8c23a57026cd40308e
ajkline <48300075+ajkline@users.noreply.github.com>
2019-10-28 16:17:14 -0400

    Added Drop Down Menus
    Added the 3 drop down menus, still need to space them apart

--- SMCM_Fall_2019_Full_Stack_Project/wwwroot/js/exampleReactComponent.jsx
+++ SMCM_Fall_2019_Full_Stack_Project/wwwroot/js/exampleReactComponent.jsx
@@ -20,10 +20,44 @@
         return (
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-3"></div>
+                    <div class="col-md-3">
+                    </div>
                     <div class="col-md-6">
+                        <div class="row">
+                            <div>
+                            <h1 className={"select-homepage"}>Genre:</h1>
+                                <select className={"select-homepage"} name={"Genre"} defaultValue={"Any"}>
+                                <option value="Any">Any Genre</option>
+                                <option value="Action">Action</option>
+                                <option value="Adventure">Adventure</option>
+                                <option value="Puzzle">Puzzle</option>
+                                <option value="Simulation">Simulation</option>
+                                <option value="Sports">Sports</option>
+                            </select>
+                        </div>
+                            <div>
+                            <h2 className={"select-homepage"}>ESRB Rating:</h2>
+                            <select className={"select-homepage"} name={"Rating"} defaultValue={"Any"}>
+                                    <option value="Any">Any Rating</option>
+                                    <option value="E">Everyone</option>
+                                    <option value="E10">Everyone 10+</option>
+                                    <option value="T">Teen</option>
+                                    <option value="M">Mature</option>
+                            </select>
+                        </div>
+                            <div>
+                        <h3 className={"select-homepage"}>Platform:</h3>
+                            <select className={"select-homepage"} name={"Platform"} defaultValue={"Any"}>
+                                <option value="Any">Any Platform</option>
+                                <option value="PC">PC</option>
+                                <option value="Xbox">Xbox</option>
+                                <option value="Playstation">Playstation</option>
+                                <option value="Switch">Switch</option>
+                            </select>
+                        </div> 
                         <h3>{this.state.text}</h3>
                         <button className={"btn-block"} onClick={() => this.ajaxTest(this)}> Press me </button>
+                        </div>
                     </div>
                     <div class="col-md-3"></div>
                 </div>
