7a13554a70cf022a48cfebb69b4303c389e33617
ZSLambert <41171374+ZSLambert@users.noreply.github.com>
2019-10-28 23:03:09 -0400

    added the delete account button

---
+++ SMCM_Fall_2019_Full_Stack_Project/wwwroot/js/accountPage.jsx
@@ -0,0 +1,12 @@
+﻿class Account extends React.Component {
+
+    render() {
+        return (
+            <div align ="center">
+                <button className={"btn-delete"}> Delete Account </button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Account />, document.getElementById('content'));
