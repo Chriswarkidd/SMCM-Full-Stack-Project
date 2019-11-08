13d345b46897ac2b431d03842d3d68f14c7db6e5
Chriswarkidd <48682852+Chriswarkidd@users.noreply.github.com>
2019-11-07 20:41:00 -0500

    Removed old DB Password from files

--- SMCM_Fall_2019_Full_Stack_Project/Models/WgsipContext.cs
+++ SMCM_Fall_2019_Full_Stack_Project/Models/WgsipContext.cs
@@ -19,10 +19,10 @@ namespace SMCM_Fall_2019_Full_Stack_Project.Models
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             //use this when working with a local database
-            //options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             
             //use this when working with the production database. (Should only be when publishing)
-            options.UseSqlServer("Data Source=full-stack-2019.database.windows.net;Initial Catalog=Full_Stack_2019;User ID=Full_Stack_SMCM_2019;Password=Admin1123!;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            //options.UseSqlServer("Data Source=full-stack-2019.database.windows.net;Initial Catalog=Full_Stack_2019;User ID=Full_Stack_SMCM_2019;Password=*******;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
         public WgsipContext(DbContextOptions options) : base(options)
         {
