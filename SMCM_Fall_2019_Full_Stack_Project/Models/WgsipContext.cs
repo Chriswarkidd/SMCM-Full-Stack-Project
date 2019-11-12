2093fc0eec586895bd70d7f5526dcf252cf6c531
Chriswarkidd <48682852+Chriswarkidd@users.noreply.github.com>
2019-11-12 14:11:24 -0500

    Added logo things

--- SMCM_Fall_2019_Full_Stack_Project/Models/WgsipContext.cs
+++ SMCM_Fall_2019_Full_Stack_Project/Models/WgsipContext.cs
@@ -20,7 +20,7 @@ namespace SMCM_Fall_2019_Full_Stack_Project.Models
         {
             //use this when working with a local database
             options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            
+
             //use this when working with the production database. (Should only be when publishing)
             //options.UseSqlServer("Data Source=full-stack-2019.database.windows.net;Initial Catalog=Full_Stack_2019;User ID=Full_Stack_SMCM_2019;Password=*******;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
