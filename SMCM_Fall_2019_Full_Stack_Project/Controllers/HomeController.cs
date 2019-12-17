7f6de7f5e3793e5aca324d23869def82c785a12f
ajkline <48300075+ajkline@users.noreply.github.com>
2019-12-17 16:36:09 -0500

    fixed homecontroller on my branch
    was missing import statements

--- SMCM_Fall_2019_Full_Stack_Project/Controllers/HomeController.cs
+++ SMCM_Fall_2019_Full_Stack_Project/Controllers/HomeController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
